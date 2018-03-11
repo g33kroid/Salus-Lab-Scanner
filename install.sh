b99b2074b450dc8f256f90036ea3d24d6778a574
Mina Ghaly <mina.abkarino@hotmail.com>
2018-03-11 21:30:58 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 
+apt-get install curl -y
+echo "deb https://apache.bintray.com/couchdb-deb jessie main" >> /etc/apt/sources.list
 
-sudo echo "deb https://apache.bintray.com/couchdb-deb stretch main" >> /etc/apt/sources.list
+curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc \
+    | sudo apt-key add -
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install couchdb
-pip install termcolor couchdb
+sudo apt-get --no-install-recommends -y install \
+    build-essential pkg-config erlang \
+    libicu-dev libmozjs185-dev libcurl4-openssl-dev -y
+
+
+sudo apt-get install couchdb -y
+systemctl start couchdb
+
+pip install termcolor couchdb python-nmap
+
+curl http://127.0.0.1:5984/
+
+apt install git -y 
+
+git clone https://github.com/mxamusic/Salus-Lab-Scanner.git ~/Desktop/SalusLabScanner
+
+python ~/Desktop/SalusLabScanner/inspector.py
 
