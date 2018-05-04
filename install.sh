4f548f404f673b71b8b96feb4c3bfe04e231744c
Mina Ghaly <mina.abkarino@hotmail.com>
2018-05-04 13:23:37 -0700

    Adding pip
    adding get pip for kali distro

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo echo "deb https://apache.bintray.com/couchdb-deb stretch main" >> /etc/apt/sources.list
 
 sudo apt-get update
-
+python get-pip.py
 sudo apt-get install couchdb
 pip install termcolor couchdb
 
